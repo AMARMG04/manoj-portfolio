7d00bb96294e209ea8f5aa674df041b86228e19d
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2024-12-10 15:09:42 +0530

    Mobile Responsiveness

--- src/app/components/Marquee.jsx
+++ src/app/components/Marquee.jsx
@@ -4,7 +4,7 @@ const Marquee = () => {
   const images = ["/assets/Next.svg","/assets/React.svg", "/assets/Nodejs.svg", "/assets/MDB.svg", "/assets/Vercel.svg"];
   return (
     <div className="h-fit w-full bg-[#0b0b0b] dark:bg-neutral-800">
-      <div className="flex flex-wrap justify-around gap-8">
+      <div className="flex flex-wrap justify-around gap-6 lg:gap-8 py-4 px-8">
         {images.map((image, index) => (
           <Image
             src={image}
@@ -12,7 +12,7 @@ const Marquee = () => {
             alt="hero"
             width={1000}
             height={1000}
-            className="w-24 h-24 lg:w-32 lg:h-32 object-contain"
+            className="w-20 h-20 lg:w-32 lg:h-32 object-contain"
           />
         ))}
       </div>
