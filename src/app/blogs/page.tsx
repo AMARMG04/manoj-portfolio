f0a1c6224d5ef86846257060f89d23b872d7a657
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2024-12-10 12:59:31 +0530

    Fourth Commit

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-[600px]'>
+      <h1 className='font-abster text-[64px]'>UNDER DEVELOPMENT</h1>
+    </div>
+  )
+}
+
+export default page
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,18 +5,18 @@ const Footer = () => {
   const Links = [
     {
       title: "Instagram",
-      link: "",
+      link: "https://www.instagram.com/the_boorish_mind/",
     },
     {
       title: "LinkedIn",
-      link: "",
+      link: "https://www.linkedin.com/in/manoj-mg-3350b6249/",
     },
   ];
   return (
     <div className="h-20 w-full bg-[#0b0b0b] dark:bg-neutral-800 px-40">
         <div className="flex flex-row gap-10 justify-end items-center h-full text-white">
           {Links.map((link, index) => (
-            <Link href={link.link} key={index}>
+            <Link href={link.link} key={index} target="_blank">
               <p className="font-nohemi_m text-lg">{link.title}</p>
             </Link>
           ))}
